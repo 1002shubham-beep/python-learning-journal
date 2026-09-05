b20510d549b785a2961596cb84e0ba0d6e55783a
Shubham <1002shubham@gmail.com>
2026-09-06 00:48:01 +0530

    Improved Concession-Stand program

--- Projects/concession_stand_program.py
+++ Projects/concession_stand_program.py
@@ -1,25 +1,39 @@
-menu={
-    "Popcorn":"$1.00",
-    "Hot Dog":"$2.00",
-    "Giant Pretzel":"$2.00",
-    "Asst Candy":"$1.00",
-    "Soda":"$1.00",
-    "Bottled Water":"$1.00"
+#Concession stand program
+
+menu = {
+    "pizza" : 3.00,
+    "nachos": 4.50,
+    "popcorn": 6.00,
+    "fries": 2.50,
+    "chips": 1.00,
+    "pretzel":3.50,
+    "soda": 3.00,
+    "lemonade":4.25
 }
+
 cart = []
 total = 0
-print("-------------MENU-------------")
-for key, value in menu.items():
-    print(f"{key:15}:{value}")
-print("------------------------------")
 
+print("-----------------Menu-----------------")
+for key, value in menu.items():
+    print(f"{key:10}:${value:.2f}")
+print("-----------------------------------------")
 while True:
-    food = input("Select an item (q to quit): ").lower()
-    print(f"Your cart: {cart}")
-    if food  == "q":
+    food = input("What would you like to order?(Q to quit): ").lower()
+    if food == "q":
         break
     elif menu.get(food) is not None:
         cart.append(food)
 
-for food in cart:
-    total += menu.get(food) 
+for item in cart:
+    total = total + menu.get(item)
+
+
+print("----------------Cart----------------")
+for item in cart:
+    if menu.get(item) is not None:
+        print(item,end=", ")
+    else:
+        continue
+print()
+print(f"Your total is: ${total:.2f}")
