05217046f2c164e377fc696634b4aad5e518ab46
Shubham <1002shubham@gmail.com>
2026-09-06 20:39:22 +0530

    Stop tracking unfinished program, update gitignore

---
+++ 06-functions/functions.py
@@ -0,0 +1,12 @@
+# function = A block of reusable code
+#            place () after the function name to invoke it
+
+
+def happy_birthday_song():
+    print("Happy Birthday to you")
+    print("You are old!")
+    print("Happy Birthday to you")
+
+for i in range(0,3):
+    happy_birthday_song()
+    print()
